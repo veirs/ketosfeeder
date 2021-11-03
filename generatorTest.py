08f42ab9ad3ec39c3bcbb62513382b6c403cf00a
Fabio Frazao <fsfrazao@gmail.com>
2021-11-03 14:38:37 -0300

    generatorTest.py-> Load the data and annotation tables instead of their paraent groups; Reduce the validata batch size to 32.

--- generatorTest.py
+++ generatorTest.py
@@ -14,16 +14,18 @@ h5filename = 'db_h5__file'
 # open h5d database for reading
 db = dbi.open_file(h5filename, 'r')
 print(db)
-train_data = dbi.open_table(db, "/train")
-val_data = dbi.open_table(db, "/eval")
-test_data = dbi.open_table(db, "/test")
+train_data = dbi.open_table(db, "/train/table_data")
+train_annot = dbi.open_table(db,"/train/table_annot")
+val_data = dbi.open_table(db, "/eval/table_data")
+val_annot = dbi.open_table(db,"/eval/table_annot")
+test_data = dbi.open_table(db, "/test/table_data")
+test_annot = dbi.open_table(db, "/test/table_annot")
 
 
 
-train_generator = BatchGenerator(batch_size=128, data_table=train_data, annot_in_data_table=True,
-                                  shuffle=True, refresh_on_epoch_end=True)
+train_generator = BatchGenerator(batch_size=128, data_table=train_data, annot_table=train_annot, annot_in_data_table=False,shuffle=True, refresh_on_epoch_end=True)
 
-val_generator = BatchGenerator(batch_size=128, data_table=val_data, annot_in_data_table=True,
+val_generator = BatchGenerator(batch_size=32, data_table=val_data, annot_in_data_table=True,
                                  shuffle=True, refresh_on_epoch_end=False)
 
 
